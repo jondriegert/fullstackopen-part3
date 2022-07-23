5cf9f5a71967461da644ce83e86ad6df9f7f539a
Jon Driegert <jondriegert@gmail.com>
2022-07-22 21:28:26 -0700

    exercise 3.2

--- index.js
+++ index.js
@@ -34,6 +34,15 @@ app.get('/api/persons', (request, response) => {
   response.json(persons);
 });
 
+app.get('/info', (request, response) => {
+  console.log(request);
+  response.send(
+    `<div>Phonebook has info for ${
+      persons.length
+    } people</div><br><div>${Date()}</div>`
+  );
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
