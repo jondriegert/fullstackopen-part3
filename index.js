c2aaa0ef296e8dface329ae18eb2999a570f99e1
Jon Driegert <jondriegert@gmail.com>
2022-07-24 20:12:10 -0700

    exercise 3.14

--- index.js
+++ index.js
@@ -111,21 +111,20 @@ app.post('/api/persons', (request, response) => {
     });
   }
 
-  if (persons.some((p) => p.name === body.name)) {
-    return response.status(400).json({
-      error: 'name must be unique',
-    });
-  }
+  // if (persons.some((p) => p.name === body.name)) {
+  //   return response.status(400).json({
+  //     error: 'name must be unique',
+  //   });
+  // }
 
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
 const PORT = process.env.PORT;
