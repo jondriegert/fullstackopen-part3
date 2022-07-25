78a0bbd064cc59fa74ea79a604780f75ce35542a
Jon Driegert <jondriegert@gmail.com>
2022-07-25 08:43:21 -0700

    exercise 3.16

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.get('/api/persons/:id', (request, response) => {
   }
 });
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then((result) => {
       response.status(204).end();
@@ -127,6 +127,18 @@ app.post('/api/persons', (request, response) => {
     response.json(savedPerson);
   });
 });
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.name);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 
